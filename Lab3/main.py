8cfe535dd04598dd4b7032526d734d083a596ebf
AndreiApricopoai <andreiapricopoai15@gmail.com>
2023-10-24 00:46:01 +0300

    Finish Homework 3

--- Lab3/main.py
+++ Lab3/main.py
@@ -1,6 +1,5 @@
-
-
 # Laboratory 3
+
 # Ex1 - Write a function that receives as parameters two lists a and b
 # and returns a list of sets containing: (a intersected with b, a reunited with b, a - b, b - a)
 def list_operations(a: list, b:list) -> tuple:
@@ -12,7 +11,7 @@ def list_operations(a: list, b:list) -> tuple:
     a_minus_b = list(a_set - b_set)
     b_minus_a = list(b_set - a_set)
 
-    return (intersection, union, a_minus_b, b_minus_a)
+    return intersection, union, a_minus_b, b_minus_a
 
 
 a = [1, 2, 3, 4, 5]
@@ -110,3 +109,64 @@ data = {"key1": "come inside, it's too cold out", "key3": "this is not valid"}
 result = validate_dict(rules, data)
 print(result)  # Should print False
 
+
+# Ex6 - Write a function that receives as a parameter a list and returns a tuple (a, b),
+# representing the number of unique elements in the list, and b representing the number
+# of duplicate elements in the list (use sets to achieve this objective).
+def count_unique_duplicate_elements(input_list: list) -> tuple[int, int]:
+    unique_elements = set(input_list)
+    duplicate_elements = len(input_list) - len(unique_elements)
+    return len(unique_elements), duplicate_elements
+
+
+example_list = [1, 2, 3, 4, 2, 5, 6, 3, 7]
+result = count_unique_duplicate_elements(example_list)
+print(result)
+
+
+# Ex7 - Write a function that receives a variable number of sets and returns a dictionary with
+# the following operations from all sets two by two: reunion, intersection, a-b, b-a.
+# The key will have the following form: "a op b", where a and b are two sets,
+# and op is the applied operator: |, &, -.
+def set_operations(*sets: set) -> dict[str, set]:
+    result = {}
+    for i in range(len(sets)):
+        for j in range(i + 1, len(sets)):
+            result[str(sets[i]) + " | " + str(sets[j])] = sets[i] | sets[j]
+            result[str(sets[i]) + " & " + str(sets[j])] = sets[i] & sets[j]
+            result[str(sets[i]) + " - " + str(sets[j])] = sets[i] - sets[j]
+    return result
+
+
+print(set_operations({1,2}, {2, 3}))
+
+
+# Ex8 - Write a function that receives a single dict parameter named mapping.
+# This dictionary always contains a string key "start". Starting with the value of this key
+# you must obtain a list of objects by iterating over mapping in the following way: the value
+# of the current key is the key for the next value, until you find a loop (a key that was visited before).
+# The function must return the list of objects obtained as previously described.
+def loop(mapping: dict[str, str]) -> list[str]:
+    result = []
+    current_key = mapping['start']
+    while current_key not in result:
+        result.append(current_key)
+        current_key = mapping[current_key]
+    return result
+
+
+print(loop({'start': 'a', 'b': 'a', 'a': '6', '6': 'z', 'x': '2', 'z': '2', '2': '2', 'y': 'start'}))
+
+
+# Ex9 - Write a function that receives a variable number of positional arguments and a variable
+# number of keyword arguments and will return the number of positional arguments whose values
+# can be found among keyword arguments values.
+def positional_keywords_counter(*positionals, **keywords) -> int:
+    count = 0
+    for positional in positionals:
+        if positional in keywords.values():
+            count = count + 1
+    return count
+
+
+print(positional_keywords_counter(1, 2, 3, 4, x=1, y=2, z=3, w=5))
